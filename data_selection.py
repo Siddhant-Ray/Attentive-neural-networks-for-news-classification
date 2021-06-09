16b89292194d3c1b9a3df3b0ce9c5807b2e31159
Siddhant Ray <siddhant.r98@gmail.com>
2021-06-09 20:41:17 +0200

    Updates week 2

--- data_selection.py
+++ data_selection.py
@@ -35,15 +35,21 @@ for _file in list_of_files:
 num_categories = len(all_categories)
 
 count_of_news_category = {}
+average_words_per_number_of_samples = []
 
 #Counting news items per category of news 
 for key in category_news.keys():
     item = category_news.get(key)
     number_of_news_items = len(item)
     count_of_news_category[key] = number_of_news_items
+    count_of_words = 0
+    for sentence in item:
+        count_of_words += len(sentence.split(" "))
+    average_words_per_number_of_samples.append(count_of_words/number_of_news_items)
 
-print(count_of_news_category["CRIME"])
-print(category_news["CRIME"][0:2])
+#print(count_of_news_category["WELLNESS"])
+#print(category_news["CRIME"][0:2])
+#print(average_words_per_number_of_samples)
 
 import random
 import matplotlib
@@ -55,7 +61,16 @@ value = count_of_news_category.values()
 plt.bar(category, value)
 plt.xticks(rotation=90)
 plt.tight_layout()
-plt.savefig('test.png')
+plt.savefig('Number_of_articles_per_category.png')
+plt.show()
+
+fig1 = plt.figure()
+category = count_of_news_category.keys()
+value = average_words_per_number_of_samples
+plt.bar(category, value)
+plt.xticks(rotation=90)
+plt.tight_layout()
+plt.savefig('Words_per_category.png')
 plt.show()
 
 import torch
@@ -78,11 +93,15 @@ def sentenceToTensor(sentence):
 
 category_plus_news_list = []
 
+from nltk.corpus import stopwords
+
 for key in category_news.keys():
     for news_item in category_news[key]:
+        #news_item = [word for word in news_item.split(" ") if word not in stopwords.words('english')]
+        #news_item = " ".join(word for word in news_item)
         category_plus_news_list.append((key, news_item))
 
-#print(category_plus_news_list[5][1])
+print(category_plus_news_list[0])
 
 def pairedTensor(pair):
     #pair is element in category_plus_news_list
@@ -90,7 +109,55 @@ def pairedTensor(pair):
     output_tensor = sentenceToTensor(pair[0])
     return (input_tensor, output_tensor)
 
-print(pairedTensor(category_plus_news_list[100]))
+#print(pairedTensor(category_plus_news_list[100]))
+
+unique_words = []
+
+'''for pair in category_plus_news_list:
+    for word in pair[1].split(" "):
+        if word.lower() not in unique_words:
+            unique_words.append(word.lower())
+
+print(unique_words[0:50])'''
+
+from sentence_transformers import SentenceTransformer
+model = SentenceTransformer("bert-base-nli-mean-tokens")
+
+sentence_embeddings = model.encode(category_plus_news_list[0][1])
+print(category_plus_news_list[0][1])
+print(sentence_embeddings)
+
+from models import InferSent
+model_version = 2
+MODEL_PATH = "encoder/infersent%s.pkl" % model_version
+params_model = {'bsize': 64, 'word_emb_dim': 300, 'enc_lstm_dim': 2048,
+                'pool_type': 'max', 'dpout_model': 0.0, 'version': model_version}
+model = InferSent(params_model)
+model.load_state_dict(torch.load(MODEL_PATH))
+
+
+use_cuda = True
+model = model.to(torch.device('cuda:0')) if use_cuda else model
+
+W2V_PATH = 'fastText/crawl-300d-2M.vec'
+model.set_w2v_path(W2V_PATH)
+model.build_vocab_k_words(K=1000000)
+
+list_of_sentences = []
+
+for pair in category_plus_news_list:
+    if pair[0]=='BUSINESS':
+        sentences = pair[1].split(". ")
+        for sentence in sentences:
+            list_of_sentences.append(sentence)
+
+#print(list_of_sentences[0:10])
+
+embeddings = model.encode(list_of_sentences, bsize=128, tokenize=False, verbose=True)
+print('nb sentences encoded : {0}'.format(len(embeddings)))
+print(embeddings[0:10])
+
+
 
 
 class RNN(nn.Module):
