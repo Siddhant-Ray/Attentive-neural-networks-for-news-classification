3531e2b1e40d3b3d200bc737af4a7880c902e391
Siddhant Ray <siddhant.r98@gmail.com>
2021-06-09 20:41:16 +0200

    minor updates

--- data_selection.py
+++ data_selection.py
@@ -25,6 +25,7 @@ all_categories = []
 
 list_of_files = os.listdir("news_data/")
 
+#Dictionary which maps every category of news to it's description
 for _file in list_of_files:
     category = _file.split(".")[0]
     all_categories.append(category)
@@ -35,6 +36,7 @@ num_categories = len(all_categories)
 
 count_of_news_category = {}
 
+#Counting news items per category of news 
 for key in category_news.keys():
     item = category_news.get(key)
     number_of_news_items = len(item)
@@ -42,18 +44,6 @@ for key in category_news.keys():
 
 print(count_of_news_category["CRIME"])
 print(category_news["CRIME"][0:2])
-
-
-def findIndexOfWordInSentence(sentence):
-    indexOfWord = {}
-    for index, word in enumerate(sentence):
-        indexOfWord[word] = index
-    return list(indexOfWord.values())
-
-import torch
-import torch.nn as nn 
-from torch import optim
-import torch.functional as F 
 
 import random
 import matplotlib
@@ -68,10 +58,42 @@ plt.tight_layout()
 plt.savefig('test.png')
 plt.show()
 
+import torch
+import torch.nn as nn 
+from torch import optim
+import torch.functional as F 
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+#print(device)
+
+def findIndexOfWordInSentence(sentence):
+    indexOfWord = {}
+    for index, word in enumerate(sentence):
+        indexOfWord[word] = index
+    return list(indexOfWord.values())
+
+def sentenceToTensor(sentence):
+    indexes = findIndexOfWordInSentence(sentence)
+    return torch.tensor(indexes , dtype = torch.long, device = device).view(-1, 1)
+
+category_plus_news_list = []
+
+for key in category_news.keys():
+    for news_item in category_news[key]:
+        category_plus_news_list.append((key, news_item))
+
+#print(category_plus_news_list[5][1])
+
+def pairedTensor(pair):
+    #pair is element in category_plus_news_list
+    input_tensor = sentenceToTensor(pair[1])
+    output_tensor = sentenceToTensor(pair[0])
+    return (input_tensor, output_tensor)
+
+print(pairedTensor(category_plus_news_list[100]))
 
 
-
-
-
-
-
+class RNN(nn.Module):
+    def __init__(self, input_size, hidden_size):
+        super(RNN, self).__init__()
+        self.hidden_size = hidden_size
